823b63f59a37d54caa52d3dd6806c7940717a4cd
Jackenson BAPTISTE <bapjacky@yahoo.fr>
2024-07-29 23:50:06 -0400

    Mise a jour

--- app/src/main/java/com/miage/mbds/diceroller/MainActivity.kt
+++ app/src/main/java/com/miage/mbds/diceroller/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.exemple.diceroller
+package com.miage.mbds.diceroller
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -8,8 +8,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.exemple.diceroller.R
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
